366a35ef45f3356b24c4253cdcf183efcd913231
Tarun Vaghela <59139915+Tarun-Vaghela90@users.noreply.github.com>
2025-09-17 15:51:30 +0530

    Update push.bat

--- push.bat
+++ push.bat
@@ -1,12 +1,1 @@
-@echo off
 
-rem A simple script to automate git add, commit, and push
-
-set /p commitMessage="Enter your commit message: "
-
-git add .
-git commit -m "%commitMessage%"
-git push origin main
-
-echo Changes pushed to GitHub!
-pause
